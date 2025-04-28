62aed20740f368aa30383d89eaebd2599de0a361
Adilzhan Slyamgazy <herztard@gmail.com>
2025-04-28 23:20:27 +0500

    banners + s3

---
+++ Dockerfile
@@ -0,0 +1,24 @@
+FROM python:3.13
+
+ENV PYTHONDONTWRITEBYTECODE=1
+ENV PYTHONUNBUFFERED=1
+
+ENV HOME=/home/app
+ENV APP_HOME=/code
+RUN mkdir $APP_HOME
+WORKDIR $APP_HOME
+
+RUN apt-get update && apt-get install -y --no-install-recommends \
+        gcc \
+        libpq-dev \
+    && rm -rf /var/lib/apt/lists/*
+
+COPY requirements.txt $APP_HOME
+RUN pip install --upgrade pip
+RUN pip install -r requirements.txt
+
+COPY . $APP_HOME
+
+RUN python3 manage.py collectstatic --noinput
+
+CMD ["python", "manage.py", "runserver", "0.0.0.0:8000"]
--- archiq_backend/settings.py
+++ archiq_backend/settings.py
@@ -24,12 +24,13 @@ load_dotenv()
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv("SECRET_KEY")
+SECRET_KEY = os.getenv("DHANGO_SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv("DHANGO_DEBUG", False)
 
-ALLOWED_HOSTS = []
+hosts_str = os.environ.get('DJANGO_ALLOWED_HOSTS', '')
+ALLOWED_HOSTS = hosts_str.split()
 
 # PSKZ ENV VARIABLES
 AWS_ACCESS_KEY_ID = os.getenv('S3_ACCESS_KEY')
